fef1e8d073f6f7a7311fc1843462a33185cd7c22
clarkdenunzio <clark@clarkdenunzio.com>
2017-06-08 20:54:32 -1000

    i can access the choices on click

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('sanity check');
+//console.log('sanity check');
+var reqListener = function(){
+
+  console.log("dog");
+
+  var category = document.getElementById('resourceType');
+  category = category.options[category.selectedIndex].innerHTML;
+
+  console.log(category);
+
+  var choice = document.getElementById('resourceId').value;
+
+  console.log(choice);
+
+};
+
+document.getElementById('requestResourceButton').addEventListener('click', reqListener);
+
