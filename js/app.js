6c3feb0a866494c3f835680dcbc024efa17e5955
clarkdenunzio <clark@clarkdenunzio.com>
2017-06-08 21:34:42 -1000

    can access by url and pass to switch statement

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //sanity check 1
 //console.log('sanity check');
-var reqListener = function(){
+var clicky = function(){
 
   //sanity check 2
   //console.log("dog");
@@ -19,7 +19,54 @@ var reqListener = function(){
   var url = `http://swapi.co/api/${category}/${choice}/`;
   console.log(url);
 
+  var reqListener = function(){
+
+    var response = JSON.parse(this.responseText);
+
+    console.log(response);
+
+    switch(category){
+
+      case 'people':
+        var name = document.createElement('h2');
+        name.innerHTML = response.name;
+        document.getElementById('contentContainer').appendChild(name);
+        var gender;
+        var species;
+
+        break;
+
+      case 'planets':
+        var name = document.createElement('h2');
+        var terrain;
+        var population;
+        var filmList;
+
+        break;
+
+      case 'starships':
+        var name = document.createElement('h2');
+        var manufacturer;
+        var shipClass;
+        var filmList;
+
+        break;
+
+      default:
+        var errorMessage = document.createElement('h1');
+        errorMessage.innerHTML = "ERROR";
+        document.getElementById('contentContainer').appendChild(errorMessage);
+        break;
+    }
+
+  };
+
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', reqListener);
+  oReq.open('GET', url);
+  oReq.send();
+
 };
 
-document.getElementById('requestResourceButton').addEventListener('click', reqListener);
+document.getElementById('requestResourceButton').addEventListener('click', clicky);
 
